8a8ec111701417540a33d4f59fec4fae50897969
Prathmesh Kamble <prathmesh2001kamble@gmail.com>
2025-01-03 11:59:29 +0530

    UI Added

---
+++ UI/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { Button } from '@mui/material';
+
+
+function App() {
+  return (
+    <Button variant="contained" color="primary">
+    Material UI Button
+  </Button>
+  );
+}
+
+export default App;
